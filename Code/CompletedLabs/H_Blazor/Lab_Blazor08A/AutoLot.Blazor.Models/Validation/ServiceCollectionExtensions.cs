ed881497b587a9f52e360691ece45bc224c88d5f
Philip Japikse <japikse@outlook.com>
2025-12-19 14:28:19 -0500

    fixed typo, remove extra code folder

--- Code/CompletedLabs/H_Blazor/Lab_Blazor08A/AutoLot.Blazor.Models/Validation/ServiceCollectionExtensions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AutoLot.Blazor.Models.Validation;
-
-public static class ServiceCollectionExtensions
-{
-    public static IServiceCollection AddModelValidation(this IServiceCollection services)
-        => services.AddValidation();
-}
--- Code/CompletedLabs/H_Blazor/Lab_Blazor08A/AutoLot.Blazor/Services/Validation/CustomFieldClassProvider.cs
+++
@@ -1,7 +0,0 @@
-namespace AutoLot.Blazor.Services.Validation;
-
-internal class CustomFieldClassProvider : FieldCssClassProvider
-{
-    public override string GetFieldCssClass(EditContext editContext, in FieldIdentifier fieldIdentifier) =>
-        editContext.IsValid(fieldIdentifier) ? "validField" : "invalidField";
-}
